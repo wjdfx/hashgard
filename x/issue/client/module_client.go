78ba6f3dc3e76b17dd97eb3f3472ad4a7be5079e
Joe He <j.he@hashgard.com>
2019-04-08 13:58:11 +0800

    export issue module client

--- x/issue/client/module_client.go
+++ x/issue/client/module_client.go
@@ -2,10 +2,11 @@ package client
 
 import (
 	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/hashgard/hashgard/x/issue"
-	issueCli "github.com/hashgard/hashgard/x/issue/client/cli"
 	"github.com/spf13/cobra"
 	"github.com/tendermint/go-amino"
+
+	issueCli "github.com/hashgard/hashgard/x/issue/client/cli"
+	"github.com/hashgard/hashgard/x/issue/domain"
 )
 
 // ModuleClient exports all client functionality from this module
@@ -20,14 +21,22 @@ func NewModuleClient(storeKey string, cdc *amino.Codec) ModuleClient {
 
 // GetTxCmd returns the transaction commands for this module
 func (mc ModuleClient) GetTxCmd() *cobra.Command {
-	govTxCmd := &cobra.Command{
-		Use:   issue.ModuleName,
-		Short: "Issue transactions subcommands",
+	issueCmd := &cobra.Command{
+		Use:   domain.ModuleName,
+		Short: "Issue coin subcommands",
 	}
+	issueCmd.AddCommand(
+		client.PostCommands(
+			issueCli.GetCmdIssueAdd(mc.cdc),
+			issueCli.GetCmdIssueMint(mc.cdc),
+			issueCli.GetCmdIssueBurn(mc.cdc),
+			issueCli.GetCmdIssueFinishMinting(mc.cdc),
+		)...)
+	issueCmd.AddCommand(client.LineBreak)
+	issueCmd.AddCommand(
+		client.GetCommands(
+			issueCli.GetCmdQueryIssue(mc.storeKey, mc.cdc),
+		)...)
 
-	govTxCmd.AddCommand(client.PostCommands(
-		issueCli.GetCmdIssue(mc.cdc),
-	)...)
-
-	return govTxCmd
+	return issueCmd
 }
